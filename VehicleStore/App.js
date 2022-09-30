b55b339e063b02a05cce492531df9682c2ef9c4d
TharinduShantha98 <tharindushantha4@gmail.com>
2022-09-30 23:01:42 +0530

    updated login page

--- VehicleStore/App.js
+++ VehicleStore/App.js
@@ -1,13 +1,22 @@
 
 import React from 'react';
 import Login from "./Screens/Login"
+import { NativeBaseProvider, Box } from "native-base";
 
 
 
 function App(){
 
       return(
+        <NativeBaseProvider>
         <Login></Login>
+      </NativeBaseProvider>
+        
+      
+
+
+
+
       );
  
 
